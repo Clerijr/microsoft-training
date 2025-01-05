eed77022cf24944bd6205dd8521d5ca3ee6ef132
Clerivaldo <clerivaldoj@gmail.com>
2025-01-05 13:54:25 -0300

    feat: Completing fourth unit. Added account features and tests

--- go/src/bankapi/main.go
+++ go/src/bankapi/main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/msft/bank"
-)
-
 func main() {
-	fmt.Println(bank.Hello())
 }
--- go/src/bankcore/bank_test.go
+++ go/src/bankcore/bank_test.go
@@ -17,3 +17,74 @@ func TestAccount(t *testing.T) {
 		t.Error("can't create an account object without a name")
 	}
 }
+
+func TestDeposit(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John Doe",
+			Address: "123 Main St",
+			Phone:   "555-555-5555",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+
+	if account.Balance != 10 {
+		t.Error("balance is not being updated after a deposit")
+	}
+}
+
+func TestDepositInvalidAmmount(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John Doe",
+			Address: "123 Main St",
+			Phone:   "555-555-5555",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	if err := account.Deposit(-10); err == nil {
+		t.Error("only positive numbers should be allowed to deposit")
+	}
+}
+
+func TestWithdraw(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John Doe",
+			Address: "123 Main St",
+			Phone:   "555-555-5555",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(10)
+	account.Withdraw(10)
+
+	if account.Balance != 0 {
+		t.Error("balance is not being updated after a withdraw")
+	}
+}
+
+func TestStatement(t *testing.T) {
+	account := Account{
+		Customer: Customer{
+			Name:    "John",
+			Address: "123 Main St",
+			Phone:   "555-555-5555",
+		},
+		Number:  1001,
+		Balance: 0,
+	}
+
+	account.Deposit(100)
+	statement := account.Statement()
+	if statement != "1001 - John - 100" {
+		t.Error("statement doesn't have the proper format")
+	}
+}
