32fd604e84203415eccc50f0f676d5edd7981b69
Clerivaldo <clerivaldoj@gmail.com>
2025-01-05 13:22:49 -0300

    feat: Completing first unit

--- go/src/bankapi/main.go
+++ go/src/bankapi/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/msft/bank"
+)
+
+func main() {
+	fmt.Println(bank.Hello())
+}
--- go/src/bankcore/bank.go
+++ go/src/bankcore/bank.go
@@ -1,1 +1,5 @@
 package bank
+
+func Hello() string {
+	return "Workando"
+}
